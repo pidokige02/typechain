b291be75f348c0dd163a089e07a98ef56b2ba533
pidokige02 <pidokige0204@gmail.com>
2022-09-09 22:24:30 +0900

    #5.4 JSDoc (09:37)

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,23 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
+
 export function init(config) {
     return true;
 }
 
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
     return code + 1;
 }
+
+// legacy js file 의 logiq 은 건드리지 않고, type checking만 ts 처람 하고 싶을 때 사용한다.
